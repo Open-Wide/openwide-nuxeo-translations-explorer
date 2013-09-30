fef8c362c030fed7ebde9a6d63def3c1e00cb89c
Marwane Kalam-Alami <marwane.kalam-alami@openwide.fr>
2013-09-30 11:22:56 +0200

    Allow to run site locally through Jekyll

---
+++ jekyll.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# ==================
+# Runs the site locally
+# See: https://help.github.com/articles/using-jekyll-with-pages#installing-jekyll
+# ==================
+
+cat _config.yml
+jekyll serve
